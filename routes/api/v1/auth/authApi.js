540c3165b6674e5ad6a50623b80c4fb44928784e
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2019-08-25 18:44:41 -0400

    * Obtener el rol del usuario logged in

--- routes/api/v1/auth/authApi.js
+++ routes/api/v1/auth/authApi.js
@@ -6,6 +6,8 @@ var jwt = require("jsonwebtoken");
 var models = require("../../../../models");
 // Model: Usuario
 var Usuario = models.Usuario;
+// MODEL: Rol
+let rolModel = models.Rol;
 // Config
 var CONFIG = require("../../../../config/jwtConfig");
 
@@ -23,64 +25,81 @@ function login(req, res) {
   console.log("req.body", req.body);
   const { us_username, us_password } = req.body;
 
-  Usuario.findOne(
-    { where: { us_username } },
-    {
-      include: [
-        { model: fotografiaModel, as: "fotografias" },
-        { model: categoriaModel, as: "categoria" },
-        { model: proveedorModel, as: "proveedor" }
-      ]
-    }
-  )
-    .then(user => {
-      if (!user) {
-        return res
-          .status(Util.HttpCodes.HTTP_400_BAD_REQUEST)
-          .send({ msg: "Incorrect username." });
-      }
+  console.log("\n\n rolModel", rolModel);
 
-      if (!user.validPassword(us_password)) {
-        return res
-          .status(Util.HttpCodes.HTTP_400_BAD_REQUEST)
-          .send({ msg: "Incorrect password." });
-      }
-
-      // JWT: Payload
-      const { us_usuario, us_username, us_email, ro_rol } = user;
-
-      const payload = {
-        us_usuario,
-        us_username,
-        us_email,
-        ro_rol
-      };
-
-      // JWT: Generate token
-      jwt.sign(
-        payload,
-        CONFIG.SECRET_TOKEN,
-        { expiresIn: expirationTime },
-        function(err, token) {
-          if (err) {
-            console.log(err);
-            res
-              .status(Util.HttpCodes.HTTP_500_INTERNAL_SERVER_ERROR)
-              .send({ user: null, auth: false, token: "", err });
-          } else {
-            res
-              .status(Util.HttpCodes.HTTP_200_OK)
-              .send({ user: payload, auth: true, token, err: null });
-          }
-        }
-      );
+  try {
+    Usuario.findOne({
+      where: { us_username: us_username },
+      include: [{ model: rolModel, as: "rol" }]
     })
-    .catch(err => {
-      console.log(err);
-      res
-        .status(Util.HttpCodes.HTTP_500_INTERNAL_SERVER_ERROR)
-        .send({ user: null, auth: false, token: "", err });
-    });
+      .then(user => {
+        //console.log("\n\n********user", user);
+        console.log("\n\n********user.rol.dataValues", user.rol.dataValues);
+        if (!user) {
+          return res
+            .status(Util.HttpCodes.HTTP_400_BAD_REQUEST)
+            .send({ msg: "Incorrect username." });
+        }
+
+        if (!user.validPassword(us_password)) {
+          return res
+            .status(Util.HttpCodes.HTTP_400_BAD_REQUEST)
+            .send({ msg: "Incorrect password." });
+        }
+
+        // JWT: Payload
+        const {
+          us_usuario,
+          us_username,
+          us_email,
+          us_avatar,
+          us_genero,
+          ro_rol
+        } = user;
+
+        const payload = {
+          us_usuario,
+          us_username,
+          us_email
+        };
+
+        const userSimplified = {
+          us_usuario,
+          us_username,
+          us_email,
+          us_avatar,
+          us_genero,
+          ro_rol
+        };
+
+        // JWT: Generate token
+        jwt.sign(
+          payload,
+          CONFIG.SECRET_TOKEN,
+          { expiresIn: expirationTime },
+          function(err, token) {
+            if (err) {
+              console.log(err);
+              res
+                .status(Util.HttpCodes.HTTP_500_INTERNAL_SERVER_ERROR)
+                .send({ user: null, auth: false, token: "", err });
+            } else {
+              res
+                .status(Util.HttpCodes.HTTP_200_OK)
+                .send({ user: userSimplified, auth: true, token, err: null });
+            }
+          }
+        );
+      })
+      .catch(err => {
+        console.log(err);
+        res
+          .status(Util.HttpCodes.HTTP_500_INTERNAL_SERVER_ERROR)
+          .send({ user: null, auth: false, token: "", err });
+      });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 module.exports = router;
