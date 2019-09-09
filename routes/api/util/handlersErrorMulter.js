3d66dd66ce83f5f930752303a04f1a8555102e82
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2019-09-08 22:32:47 -0400

    Handler errors multer

---
+++ routes/api/util/handlersErrorMulter.js
@@ -0,0 +1,43 @@
+// HTTP CODES
+var Util = require("./httpcodes");
+
+var fnHandlerErrorMulter = function(err, maxLimitFiles) {
+  console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+  console.log("\n\n fnHandlerErrorMulter err: ");
+  console.log(err);
+
+  let result = {
+    nameError: "",
+    statusCode: Util.HttpCodes.HTTP_500_INTERNAL_SERVER_ERROR,
+    message: "",
+    code: 0,
+    field: 0,
+    storageErrors: []
+  };
+
+  if (err.name !== null && err.name !== undefined) {
+    result.nameError = err.name;
+  }
+
+  if (err.code !== null && err.code !== undefined) {
+    result.code = err.code;
+
+    if (result.code === "LIMIT_UNEXPECTED_FILE") {
+      result.message =
+        "Se estan subiendo mas archivos de los permitidos a la vez, que es " +
+        maxLimitFiles +
+        " archivo(s)";
+    }
+  }
+
+  if (err.field !== null && err.field !== undefined) {
+    result.field = err.field;
+  }
+
+  console.log(result);
+  console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+  return result;
+};
+
+module.exports = fnHandlerErrorMulter;
