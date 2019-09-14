6eccf5a8d9e5dbffd3a131325410d3e06c9c3880
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2019-09-14 13:49:45 -0400

    * Mejoras  en el handler del error multer

--- routes/api/util/handlersErrorMulter.js
+++ routes/api/util/handlersErrorMulter.js
@@ -7,11 +7,12 @@ var fnHandlerErrorMulter = function(err, folderUploads, maxLimitFiles) {
   console.log(err);
 
   let result = {
-    nameError: "",
+    nameError: "MulterError",
     statusCode: Util.HttpCodes.HTTP_500_INTERNAL_SERVER_ERROR,
-    message: "",
-    code: 0,
-    field: 0,
+    message: err.message,
+    code: err.code,
+    field: err.field,
+    stack: err.stack,
     storageErrors: []
   };
 
@@ -20,16 +21,22 @@ var fnHandlerErrorMulter = function(err, folderUploads, maxLimitFiles) {
   }
 
   if (err.code !== null && err.code !== undefined) {
-    result.code = err.code;
-
-    if (result.code === "LIMIT_UNEXPECTED_FILE") {
-      result.message =
-        "Se estan subiendo mas archivos de los permitidos a la vez, que es " +
-        maxLimitFiles +
-        " archivo(s)";
-    }
-    if (result.code === "ENOENT") {
-      result.message = "El folder " + folderUploads + " no existe";
+    switch (err.code) {
+      case "LIMIT_FILE_COUNT":
+        result.message =
+          "Se estan subiendo mas archivos de los permitidos a la vez, que es " +
+          maxLimitFiles +
+          " archivo(s)";
+        break;
+      case "LIMIT_UNEXPECTED_FILE":
+        result.message = `El campo ${err.field} no existe, por favor reviselo`;
+        break;
+      case "ENOENT":
+        result.message = "El folder " + folderUploads + " no existe";
+        break;
+      default:
+        result.message = "Error, al subir el archivo";
+        break;
     }
   }
 
