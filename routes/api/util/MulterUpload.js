69b3e60b327d9abb641f6e26e636d761dff27909
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2019-09-14 16:40:25 -0400

    * Mejoras

--- routes/api/util/MulterUpload.js
+++ routes/api/util/MulterUpload.js
@@ -3,8 +3,8 @@ var multer = require("multer");
 // Functions
 var fnBuildDiskStorage = require("../util/MulterStorages");
 
-function fnGetUploadWithDiskStorage(basePath, fieldName) {
-  const fullPath = `${basePath}/${fieldName}`;
+function fnGetUploadWithDiskStorage(fullPath) {
+  console.log("fullPath", fullPath);
   // Multer Storage
   const DiskStorage = fnBuildDiskStorage(fullPath);
   return multer({ storage: DiskStorage });
