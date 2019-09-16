eed5063c44cc03b5970f20121eaca95f2d95b776
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2019-09-15 23:58:43 -0400

    + Adicionar el co_compra y el pr_producto

--- models/relincluye.js
+++ models/relincluye.js
@@ -3,6 +3,14 @@ module.exports = (sequelize, DataTypes) => {
   const RelIncluye = sequelize.define(
     "RelIncluye",
     {
+      co_compra: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      pr_producto: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
       ri_cantidad: {
         type: DataTypes.INTEGER,
         allowNull: false,
@@ -19,6 +27,23 @@ module.exports = (sequelize, DataTypes) => {
       timestamps: false
     }
   );
-  RelIncluye.associate = function(models) {};
+  RelIncluye.associate = function(models) {
+    RelIncluye.belongsTo(models.Producto, {
+      as: "producto",
+      onDelete: "CASCADE",
+      foreignKey: {
+        name: "pr_producto",
+        allowNull: false
+      }
+    });
+    RelIncluye.belongsTo(models.Compra, {
+      as: "compra",
+      onDelete: "CASCADE",
+      foreignKey: {
+        name: "co_compra",
+        allowNull: false
+      }
+    });
+  };
   return RelIncluye;
 };
